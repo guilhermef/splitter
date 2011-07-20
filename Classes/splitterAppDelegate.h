535f7fd932c52269602187f54ee3d98f82480ab3
Guilherme Souza <guivideojob@gmail.com>
2011-07-19 21:41:41 -0300

    my files

---
+++ Classes/splitterAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  splitterAppDelegate.h
+//  splitter
+//
+//  Created by Guilherme Souza on 02/04/11.
+//  Copyright Globo.com 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface splitterAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  splitter
+//
+//  Created by Guilherme Souza on 02/04/11.
+//  Copyright Globo.com 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"splitterAppDelegate");
+    [pool release];
+    return retVal;
+}
